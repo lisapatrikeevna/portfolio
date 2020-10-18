f59f44435fabe12518bd08811214196f88e3c5f4
lisapatrikeevna <lisa15.08patrikeevna@gmail.com>
2020-10-19 02:27:09 +0300

    Updates

---
+++ precache-manifest.42f738ebc61ca8c457e1cf15a681fb7c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9907fb07a3c34af9e9f856220140d458",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "91f3dc71d0aa2764ee74",
+    "url": "/portfolio/static/css/main.306257c6.chunk.css"
+  },
+  {
+    "revision": "5b0932fe6812d69c6e3c",
+    "url": "/portfolio/static/js/2.2de7fc4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.2de7fc4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f3dc71d0aa2764ee74",
+    "url": "/portfolio/static/js/main.6d5f7256.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "23e5b36d75cb755acdf8580cae42a173",
+    "url": "/portfolio/static/media/formBG.23e5b36d.jpg"
+  },
+  {
+    "revision": "defd5d0eed71cbfb8f1a963295aab2f9",
+    "url": "/portfolio/static/media/girl.defd5d0e.png"
+  },
+  {
+    "revision": "10235e05fa62951ab6e319f1d5970d95",
+    "url": "/portfolio/static/media/logo.10235e05.png"
+  }
+]);
