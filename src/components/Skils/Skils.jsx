321f138baf52ebeedebbb64fb450127d78f9a6bb
lisapatrikeevna <lisa15.08patrikeevna@gmail.com>
2020-10-19 19:02:34 +0300

    add style on map

--- src/components/Skils/Skils.jsx
+++ src/components/Skils/Skils.jsx
@@ -18,26 +18,26 @@ export const Main = () => {
             img: "src",
             name: "some name",
             desc: "Ut ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
-            // background: "#8ec6d3"
+            background: "#8ec6d3"
         },
         {
             img: "src",
             name: "some name",
             desc: "Ut ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
-            // background: "#bbcbd7"
+            background: "#bbcbd7"
         },
         {
             img: "src",
             name: "some name",
             desc: "Ut ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
-            // background: "#dbc6dd"
+            background: "#dbc6dd"
         }
     ]
     return (
         <div className={cl.ollSkils}>
             {skils.map(s => {
-                // return <div className={cl.wrappSkil} style={`background: ${s.background}`}>
-                return <div className={cl.wrappSkil}>
+                // return <div className={cl.wrappSkil} >
+                return <div className={cl.wrappSkil} style={{background: s.background}}>
                     <span>{s.img}</span>
                     <p>{s.name}</p>
                     <div>{s.desc}</div>
